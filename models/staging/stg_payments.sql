7daed6ce6e08c18eb60ed3283a718e511de55cd3
Paul Pham <dev@dbtlabs.com>
2023-08-22 17:48:34 +0000

    shouldn't trigger on draft pull request?

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -14,7 +14,7 @@ renamed as (
         id as payment_id,
         order_id,
         payment_method,
-
+        -- add a comment about something here to trigger a new run
         --`amount` is currently stored in cents, so we convert it to dollars
         amount / 100 as amount
 
