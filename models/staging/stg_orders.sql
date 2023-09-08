840111827782effde82811f5491edc5761f78f55
Paul Pham <dev@dbtlabs.com>
2023-09-08 23:05:36 +0000

    test commit

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,8 +1,7 @@
 with source as (
 
     {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
+    Update this comment here for it to trigger something
     #}
     select * from {{ ref('raw_orders') }}
 
